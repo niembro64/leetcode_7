c34a5b042bec0392f5ed883408ae2a2134652717
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-07-18 23:38:19 -0400

    a

--- leetcode_7.js
+++ leetcode_7.js
@@ -1,25 +1,16 @@
 function reverse(x) {
-    if (x < -1 * Math.pow(2, 31) || x > Math.pow(2, 31) - 1) {
-        return 0;
-    }
     var pos = true;
     if (x < 0) {
         x *= -1;
         pos = false;
     }
-    var e = 0;
-    while (Math.pow(10, e) - 1 < x) {
-        e++;
-    }
-    e--;
-    // console.log("x", x, "e", e);
-    var temp = 0;
     var sum = 0;
-    for (var i = 0; i <= e; i++) {
-        temp = Math.floor(x / Math.pow(10, e - i));
-        x -= Math.pow(10, e - i) * temp;
-        sum += Math.pow(10, i) * temp;
-        // console.log("x", x, "e", e, "i", i, "temp", temp, "sum", sum);
+    while (x > 0) {
+        sum = sum * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    if (sum < -1 * Math.pow(2, 31) || sum > Math.pow(2, 31) - 1) {
+        return 0;
     }
     return pos ? sum : sum * -1;
 }
@@ -30,5 +21,3 @@ for (var i = 0; i < a.length; i++) {
     console.log(reverse(a[i]));
     console.log("_____________");
 }
-console.log(Math.pow(2, 31) - 1);
-console.log(1534236469);
--- leetcode_7.ts
+++ leetcode_7.ts
@@ -1,29 +1,18 @@
 function reverse(x: number): number {
-    if (x < -1 * Math.pow(2, 31) || x > Math.pow(2, 31) - 1) {
-        return 0;
-    }
-
     let pos: boolean = true;
     if (x < 0) {
         x *= -1;
         pos = false;
     }
 
-    let e: number = 0;
-    while (Math.pow(10, e) - 1 < x) {
-        e++;
-    }
-    e--;
-    // console.log("x", x, "e", e);
-    let temp: number = 0;
     let sum: number = 0;
-    for (let i = 0; i <= e; i++) {
-        temp = Math.floor(x / Math.pow(10, e - i));
-        x -= Math.pow(10, e - i) * temp;
-        sum += Math.pow(10, i) * temp;
-        // console.log("x", x, "e", e, "i", i, "temp", temp, "sum", sum);
+    while (x > 0) {
+        sum = sum * 10 + (x % 10);
+        x = Math.floor(x / 10);
     }
-
+    if (sum < -1 * Math.pow(2, 31) || sum > Math.pow(2, 31) - 1) {
+        return 0;
+    }
     return pos ? sum : sum * -1;
 }
 // 1534236469
@@ -35,6 +24,3 @@ for (let i = 0; i < a.length; i++) {
     console.log(reverse(a[i]));
     console.log("_____________");
 }
-
-console.log(Math.pow(2, 31) - 1);
-console.log(1534236469);
